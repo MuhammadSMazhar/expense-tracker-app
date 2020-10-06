af3639382cb54012520aecffd57c7fe9b2bb74b0
MuhammadShoaibMazhar <44173628+MuhammadSMazhar@users.noreply.github.com>
2020-10-06 22:41:10 +0500

    build a Expense tracker App

---
+++ src/child.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Child() {
+    return (
+        <div className="container">
+
+            <h1 className="text-center">Expense Tracker</h1>
+            <h3>YOUR BALANCE <br /> $260</h3>
+
+            <div className="expensecontainer">
+                <h3>YOUR BALANCE <br /> $260</h3>
+                <h3>YOUR BALANCE <br /> $260</h3>
+            </div>
+        </div>
+    );
+}
+
+export default Child;
